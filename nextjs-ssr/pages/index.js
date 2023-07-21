93719564ec679c86671778c9286df59a92c0552d
Albajunior <guissealba@gmail.com>
2023-07-21 16:45:09 +0200

    new-page

--- nextjs-ssr/pages/index.js
+++ nextjs-ssr/pages/index.js
@@ -1,36 +1,5 @@
-// import Head from 'next/head'
-// import Layout, { siteTitle } from '../components/layout'
-// import utilStyles from '../styles/utils.module.css'
-// import axios from 'axios';
-
-// const Tasks = ({ tasks }) => {
-//   return (
-//     <div>
-//       <h1>Liste des tâches</h1>
-//       <ul>
-//         {tasks.map((task) => (
-//           <li key={task.id}>
-//             {task.title} 
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export async function getServerSideProps() {
-//   const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-//   const tasks = response.data;
-//   return {
-//     props: { tasks },
-//   };
-// }
-
-// export default Tasks;
-
-// pages/tasks.js
-import Link from 'next/link';
-import axios from 'axios';
+import Link from "next/link";
+import axios from "axios";
 
 const Tasks = ({ tasks }) => {
   return (
@@ -39,9 +8,7 @@ const Tasks = ({ tasks }) => {
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>
-            <Link href={`/posts/${task.id}`}>
-              {task.title}
-            </Link>
+            <Link href={`/posts/${task.id}`}>{task.title}</Link>
           </li>
         ))}
       </ul>
@@ -50,11 +17,16 @@ const Tasks = ({ tasks }) => {
 };
 
 export async function getServerSideProps() {
-  const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-  const tasks = response.data;
-  return {
-    props: { tasks },
-  };
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+    const tasks = response.data;
+    return {
+      props: { tasks },
+    };
+  } catch (error) {
+    return { props: { tasks: [] } };
+  }
 }
-
 export default Tasks;
